2b3a610424b637007afb711437c76131348f3eb7
Arif Shahriyar Nader <arif.nader95@gmail.com>
2023-05-22 19:56:06 +0600

    booking reservation

--- api/index.js
+++ api/index.js
@@ -57,11 +57,7 @@ app.use((err, req,res,next) =>{
 // })
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.listen(PORT,() =>{
     connect()
